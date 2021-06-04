5cf93fba79ecabc51d42c0fb9922ae61648440a2
Pan Philipp <user16@madskill.ru>
2021-06-04 15:17:44 +0300

    Initial Commit

---
+++ proba10/proba10App.swift
@@ -0,0 +1,17 @@
+//
+//  proba10App.swift
+//  proba10
+//
+//  Created by user on 04.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct proba10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
